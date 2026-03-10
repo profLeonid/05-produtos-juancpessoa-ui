32eabfa27f11ff4fb606c2545a224810d7594911
JUAN CARLOS FONSECA PESSOA LOPES <25203642@CFP127ALUNO.LOCAL>
2026-03-10 09:42:36 -0300

    Finalizado

--- main.js
+++ main.js
@@ -3,15 +3,37 @@
 function adicionarProduto(){
     const lista = document.getElementById('lista')
     const produto = document.getElementById('produto')
+    const codigo = document.getElementById('codigo')
+    const quantidade = document.getElementById('quantidade')
+    const tbody = document.getElementById('atributos')
 
-    const span = document.createElement('span')
-    span.textContent = produto.value
-
-    span.className = " flex flex col bg-yellow-100 px8 py-2" 
-
-    if (produto.value == ""){
-        console.log('erro')
-    }else{
-        lista.appendChild(span)
+    if (
+        produto.value === "" || 
+        codigo.value === "" ||
+        quantidade.value === ""){
+        alert('erro')
     }
+    const tr = document.createElement('tr')
+
+    tr.className= " bg-white shadow-md rounded-lg houver:shadow-lg houver:bg-gray-50 transition"
+
+    tr.innerHTML = `
+        <td class="px-4 py-2 font-semibold">${codigo.value}</td>
+        <td class="px-4 py-2">${produto.value}</td>
+        <td class="px-4 py-2">${quantidade.value}</td>
+    `
+    tbody.appendChild(tr)
+}
+
+function limparProduto(){
+     const tbody = document.getElementById('atributos')
+     const ultimoProduto = tbody.lastElementChild
+
+     if(ultimoProduto){
+        ultimoProduto.remove()
+     }else{
+        alert("Nenhum produto para excluir")
+     }
+
+
 }
